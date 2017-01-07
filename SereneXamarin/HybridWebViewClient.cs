99fb70ca89943d92926550bad5c7db5c3cd86325
dFaruque <dFaruque@gmail.com>
2017-01-07 23:37:30 +0600

    maked crud generic somehow

--- SereneXamarin/HybridWebViewClient.cs
+++ SereneXamarin/HybridWebViewClient.cs
@@ -23,21 +23,6 @@ namespace SereneXamarin.Mobile
         public HybridWebViewClient(Context context) : base()
         {
             this.context = context;
-
-
-
-        }
-
-        public override void OnPageFinished(WebView view, string url)
-        {
-            base.OnPageFinished(view, url);
-            //view.LoadUrl("javascript:$(document).ajaxStart(function (event, request, settings) { " +
-            //        "ajaxHandler.ajaxBegin(); " + // Event called when an AJAX call begins
-            //        "});");
-            //view.LoadUrl("javascript:$(document).ajaxComplete(function (event, request, settings) { " +
-            //        "ajaxHandler.ajaxDone(); " + // Event called when an AJAX call ends
-            //        "});");
-
         }
 
         public override bool ShouldOverrideUrlLoading(WebView webView, string url)
@@ -51,154 +36,75 @@ namespace SereneXamarin.Mobile
             //if (IsNetworkConnected(this.context))
             //    return false;
 
-            //// This handler will treat everything between the protocol and "?"
-            //// as the method name.  The querystring has all of the parameters.
             var resources = url.Split('?');
             var requestUrl = resources[0];
-            var method = requestUrl.Substring(scheme.Length);
-            //var parameters = System.Web.HttpUtility.ParseQueryString(resources[1]);
 
-            //if (method == "UpdateLabel")
-            //{
-            //    var textbox = parameters["textbox"];
-
-            //    // Add some text to our string here so that we know something
-            //    // happened on the native part of the round trip.
-            //    var prepended = string.Format("C# says \"{0}\"", textbox);
-
-            //    // Build some javascript using the C#-modified result
-            //    var js = string.Format("SetLabelText('{0}');", prepended);
-
-            //    webView.LoadUrl("javascript:" + js);
-            //}
-
-            //if (resources.Length > 1 && method.StartsWith("http://Serenity.Mobile/Services/", StringComparison.OrdinalIgnoreCase))
-            //{
             var requestJson = System.Web.HttpUtility.UrlDecode(resources[1]);
             string responseJson = string.Empty;
+            object responseObj = null;
 
             using (var connection = GetConnection())
             {
-                if (method.Equals("Administration/Role/List", StringComparison.OrdinalIgnoreCase))
+                var mmm = requestUrl.Substring(scheme.Length).Split('/');
+                if (mmm.Length > 2)
                 {
-                    ListRequest requestObj;
-                    if (requestJson.Equals("undefined"))
-                        requestObj = new ListRequest();
-                    else
-                        requestObj = JsonConvert.DeserializeObject<ListRequest>(requestJson);
+                    var moduleName = mmm[0];
+                    var entityName = mmm[1];
+                    var methodName = mmm[2];
 
-                    var response = new RoleRepository().List(connection,
-                        requestObj);
-                    responseJson = response.ToJson();
-                }
-                else if (method.Equals("Administration/Role/Create", StringComparison.OrdinalIgnoreCase))
-                {
-
-                    var uow = new UnitOfWork(connection);
-                    var req = JsonConvert.DeserializeObject<SaveRequest<RoleRow>>(requestJson);
-                    var response = new RoleRepository().Create(uow, req);
-
-                    uow.Commit();
-
-                    responseJson = response.ToJson();
-
-                }
-                else if (method.Equals("Administration/Role/Update", StringComparison.OrdinalIgnoreCase))
-                {
-
-                    var uow = new UnitOfWork(connection);
-                    var req = JsonConvert.DeserializeObject<SaveRequest<RoleRow>>(requestJson);
-                    var response = new RoleRepository().Update(uow, req);
-
-                    uow.Commit();
-                    responseJson = response.ToJson();
-                }
-                else if (method.Equals("Administration/Role/Retrieve", StringComparison.OrdinalIgnoreCase))
-                {
-                    var req = JsonConvert.DeserializeObject<RetrieveRequest>(requestJson);
-                    var response = new RoleRepository().Retrieve(connection, req);
-
-                    responseJson = response.ToJson();
-                }
-                else if (method.Equals("Administration/Role/Delete", StringComparison.OrdinalIgnoreCase))
-                {
-
-                    var uow = new UnitOfWork(connection);
-                    var req = JsonConvert.DeserializeObject<DeleteRequest>(requestJson);
-                    var response = new RoleRepository().Delete(uow, req);
-
-                    uow.Commit();
-                    responseJson = response.ToJson();
-                }
-            }
-            //}
-            webView.LoadUrl($"javascript: submitFakeAjaxResponse('{requestUrl}', '{responseJson}');");
-
-            return true;
-        }
-        //override 
-        public override void OnReceivedClientCertRequest(WebView view, ClientCertRequest request)
-        {
-            base.OnReceivedClientCertRequest(view, request);
-
-        }
-
-        public override void OnReceivedError(WebView view, IWebResourceRequest request, WebResourceError error)
-        {
-            base.OnReceivedError(view, request, error);
-        }
-        public override void OnReceivedHttpError(WebView view, IWebResourceRequest request, WebResourceResponse errorResponse)
-        {
-            base.OnReceivedHttpError(view, request, errorResponse);
-        }
-        public override WebResourceResponse ShouldInterceptRequest(WebView view, string url)
-        {
-            var resources = url.Split('?');
-            var method = resources[0];
-            if (resources.Length > 1 && method.StartsWith("http://Serenity.Mobile/Services/", StringComparison.OrdinalIgnoreCase))
-            {
-                var requestJson = System.Web.HttpUtility.UrlDecode(resources[1]);
-                var requestObj = JsonConvert.DeserializeObject(requestJson);
-
-
-                WebResourceResponse response = null;
-                using (var connection = GetConnection())
-                {
-                    if (method.Equals("http://Serenity.Mobile/Services/Administration/Role/List", StringComparison.OrdinalIgnoreCase))
+                    dynamic repository = Activator.CreateInstance("SereneXamarin.Mobile", $"SereneXamarin.{moduleName}.Repositories.{entityName}Repository").Unwrap();
+                    if (methodName.Equals("List", StringComparison.OrdinalIgnoreCase))
                     {
-                        var data = new RoleRepository().List(connection,
-                            requestObj as ListRequest);
+                        ListRequest requestObj;
+                        if (requestJson.Equals("undefined"))
+                            requestObj = new ListRequest();
+                        else
+                            requestObj = JsonConvert.DeserializeObject<ListRequest>(requestJson);
 
-                        //var mimeType = "application/json";
-                        //var encoding = "utf-8";
-                        //Dictionary<string, string> responseHeaders = new Dictionary<string, string>();
-                        //responseHeaders.Add("Cache-Control", "private, s-maxage=0");
-                        //responseHeaders.Add("Content-Type", "application/json; charset=utf-8");
-
-                        //response = new WebResourceResponse(mimeType, encoding, GenerateStreamFromString(data.ToString()));
-                        //response.ResponseHeaders = responseHeaders;
-                        //response.SetStatusCodeAndReasonPhrase(200, "HTTP/1.1 200 OK");
-
-                        view.LoadUrl($"javascript: setItemToActiveGrid('{data.Entities.ToJson()}');");
-
+                        responseObj = repository.List(connection, requestObj);
                     }
-                    else if (method.Equals("http://Serenity.Mobile/Services/Administration/Role/Create", StringComparison.OrdinalIgnoreCase))
+                    else if (methodName.Equals("Create", StringComparison.OrdinalIgnoreCase))
                     {
 
                         var uow = new UnitOfWork(connection);
                         var req = JsonConvert.DeserializeObject<SaveRequest<RoleRow>>(requestJson);
-                        var c = new RoleRepository().Create(uow, req);
+                        responseObj = repository.Create(uow, req);
 
                         uow.Commit();
-                        view.LoadUrl($"javascript: setItemToActiveGrid('{c.ToJson()}');");
-
 
                     }
-                    return response;
+                    else if (methodName.Equals("Update", StringComparison.OrdinalIgnoreCase))
+                    {
+
+                        var uow = new UnitOfWork(connection);
+                        var req = JsonConvert.DeserializeObject<SaveRequest<RoleRow>>(requestJson);
+                        responseObj = repository.Update(uow, req);
+
+                        uow.Commit();
+                    }
+                    else if (methodName.Equals("Retrieve", StringComparison.OrdinalIgnoreCase))
+                    {
+                        var req = JsonConvert.DeserializeObject<RetrieveRequest>(requestJson);
+                        responseObj = repository.Retrieve(connection, req);
+
+                    }
+                    else if (methodName.Equals("Delete", StringComparison.OrdinalIgnoreCase))
+                    {
+
+                        var uow = new UnitOfWork(connection);
+                        var req = JsonConvert.DeserializeObject<DeleteRequest>(requestJson);
+                        responseObj = repository.Delete(uow, req);
+
+                        uow.Commit();
+                    }
+                    responseJson = JsonConvert.SerializeObject(responseObj);
+                    webView.LoadUrl($"javascript: submitFakeAjaxResponse('{requestUrl}', '{responseJson}');");
                 }
             }
-            return base.ShouldInterceptRequest(view, url);
+
+            return true;
         }
+
         public static Stream GenerateStreamFromString(string s)
         {
             MemoryStream stream = new MemoryStream();
@@ -252,21 +158,6 @@ namespace SereneXamarin.Mobile
             writeStream.Close();
         }
 
-        public override WebResourceResponse ShouldInterceptRequest(WebView view, IWebResourceRequest request)
-        {
-            if (request.Url.Path != null && request.Url.Path.Contains("Services/Administration/Role"))
-            {
-
-            }
-
-            return base.ShouldInterceptRequest(view, request);
-        }
-
-        public override void OnFormResubmission(WebView view, Message dontResend, Message resend)
-        {
-            base.OnFormResubmission(view, dontResend, resend);
-        }
-
         public static Boolean IsNetworkConnected(Context c)
         {
             ConnectivityManager connectivityManager = (ConnectivityManager)c.GetSystemService(Context.ConnectivityService);
@@ -276,64 +167,5 @@ namespace SereneXamarin.Mobile
 
             return isOnline;
         }
-
-        //    //@TargetApi(Build.VERSION_CODES.LOLLIPOP)
-        ////@Override
-
-        //public override WebResourceResponse shouldInterceptRequest(WebView view, IWebResourceRequest request)
-        //    {
-        //        Log.i(TAG, "shouldInterceptRequest path:" + request.getUrl().getPath());
-        //        WebResourceResponse returnResponse = null;
-        //        if (request.getUrl().getPath().startsWith("/cart"))
-        //        { // only interested in /cart requests
-        //            returnResponse = super.shouldInterceptRequest(view, request);
-        //            Log.i(TAG, "cart AJAX call - doing okRequest");
-        //            Request okRequest = new Request.Builder()
-        //                    .url(request.getUrl().toString())
-        //                    .post(null)
-        //                    .build();
-        //            try
-        //            {
-        //                Response okResponse = app.getOkHttpClient().newCall(okRequest).execute();
-        //                if (okResponse != null)
-        //                {
-        //                    int statusCode = okResponse.code();
-        //                    String encoding = "UTF-8";
-        //                    String mimeType = "application/json";
-        //                    String reasonPhrase = "OK";
-        //                    Map<String, String> responseHeaders = new HashMap<String, String>();
-        //                    if (okResponse.headers() != null)
-        //                    {
-        //                        if (okResponse.headers().size() > 0)
-        //                        {
-        //                            for (int i = 0; i < okResponse.headers().size(); i++)
-        //                            {
-        //                                String key = okResponse.headers().name(i);
-        //                                String value = okResponse.headers().value(i);
-        //                                responseHeaders.put(key, value);
-        //                                if (key.toLowerCase().contains("x-cart-itemcount"))
-        //                                {
-        //                                    Log.i(TAG, "setting cart item count");
-        //                                    app.setCartItemsCount(Integer.parseInt(value));
-        //                                }
-        //                            }
-        //                        }
-        //                    }
-        //                    InputStream data = new ByteArrayInputStream(okResponse.body().string().getBytes(StandardCharsets.UTF_8));
-        //                    Log.i(TAG, "okResponse code:" + okResponse.code());
-        //                    returnResponse = new WebResourceResponse(mimeType, encoding, statusCode, reasonPhrase, responseHeaders, data);
-        //                }
-        //                else
-        //                {
-        //                    Log.w(TAG, "okResponse fail");
-        //                }
-        //            }
-        //            catch (IOException e)
-        //            {
-        //                e.printStackTrace();
-        //            }
-        //        }
-        //        return returnResponse;
-        //    }
     }
 }
