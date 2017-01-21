67b9ef80fd8e6d2f067f6632d49c56191f72a8a0
dFaruque <dFaruque@gmail.com>
2017-01-21 23:23:09 +0600

    maked the web app single paged

--- SereneXamarin/SereneXamarin.Web/Modules/Common/Navigation/NavigationItems.cs
+++ SereneXamarin/SereneXamarin.Web/Modules/Common/Navigation/NavigationItems.cs
@@ -1,11 +1,12 @@
-﻿using Serenity.Navigation;
+﻿using SereneXamarin.Administration;
+using Serenity.Navigation;
 using Administration = SereneXamarin.Administration.Pages;
 
 [assembly: NavigationLink(1000, "Dashboard", url: "~/", permission: "", icon: "icon-speedometer")]
 
 [assembly: NavigationMenu(9000, "Administration", icon: "icon-screen-desktop")]
 [assembly: NavigationLink(9000, "Administration/Exceptions Log", url: "~/errorlog.axd", permission: SereneXamarin.Administration.PermissionKeys.Security, icon: "icon-ban", Target = "_blank")]
-[assembly: NavigationLink(9000, "Administration/Languages", typeof(Administration.LanguageController), icon: "icon-bubbles")]
-[assembly: NavigationLink(9000, "Administration/Translations", typeof(Administration.TranslationController), icon: "icon-speech")]
-[assembly: NavigationLink(9000, "Administration/Roles", typeof(Administration.RoleController), icon: "icon-lock")]
-[assembly: NavigationLink(9000, "Administration/User Management", typeof(Administration.UserController), icon: "icon-people")]
+[assembly: NavigationLink(9000, "Administration/Languages", "#Administration/Language", PermissionKeys.Translation, icon: "icon-bubbles")]
+[assembly: NavigationLink(9000, "Administration/Translations", "#Administration/Translation", PermissionKeys.Translation, icon: "icon-speech")]
+[assembly: NavigationLink(9000, "Administration/Roles", "#Administration/Role", PermissionKeys.Security, icon: "icon-lock")]
+[assembly: NavigationLink(9000, "Administration/User Management", "#Administration/User", PermissionKeys.Security, icon: "icon-people")]
--- SereneXamarin/SereneXamarin.Web/Modules/Common/ScriptInitialization.ts
+++ SereneXamarin/SereneXamarin.Web/Modules/Common/ScriptInitialization.ts
@@ -1,4 +1,25 @@
-﻿namespace SereneXamarin.ScriptInitialization {
+﻿var g_currentGrid: Serenity.EntityGrid<any, any>;
+
+namespace SereneXamarin.ScriptInitialization {
     Q.Config.responsiveDialogs = true;
     Q.Config.rootNamespaces.push('SereneXamarin');
+
+    window.onhashchange = (ev) => {
+        let newURL = ev.newURL.split('#');
+
+        if (newURL.length > 0) {
+            let hash = newURL[1];
+            let hash2 = hash.split('/');
+            let moduleName = hash2[0];
+            let gridName = hash2[1] + 'Grid';
+
+            let gridContainer = $('#GridDiv');
+
+            if (g_currentGrid && g_currentGrid.destroy) g_currentGrid.destroy();
+            g_currentGrid = new SereneXamarin[moduleName][gridName](gridContainer);
+
+            Q.initFullHeightGridPage(gridContainer);
+
+        }
+    };
 }
