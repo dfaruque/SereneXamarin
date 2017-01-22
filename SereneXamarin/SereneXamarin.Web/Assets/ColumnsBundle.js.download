54894bfa1522a0ba6507e857cf35e1fa55ae757b
dFaruque <dFaruque@gmail.com>
2017-01-22 15:46:43 +0600

    esd

---
+++ SereneXamarin/SereneXamarin.Web/Assets/ColumnsBundle.js.download
@@ -0,0 +1,12 @@
+﻿Q.ScriptData.set('Columns.Administration.Language', [{"name":"LanguageId","title":"Db.Administration.Language.LanguageId","editorParams":{"maxLength":10},"maxLength":10,"required":true,"width":150,"editLink":true},{"name":"LanguageName","title":"Db.Administration.Language.LanguageName","editorParams":{"maxLength":50},"maxLength":50,"required":true,"width":150,"editLink":true}]);
+
+
+
+Q.ScriptData.set('Columns.Administration.Role', [{"name":"RoleId","title":"Db.Shared.RecordId","editorType":"Integer","required":true,"insertable":false,"updatable":false,"formatterType":"Number","alignment":"right","width":65,"editLink":true,"filteringType":"Integer"},{"name":"RoleName","title":"Db.Administration.Role.RoleName","editorParams":{"maxLength":100},"maxLength":100,"required":true,"width":300,"editLink":true}]);
+
+
+
+Q.ScriptData.set('Columns.Administration.User', [{"name":"UserId","title":"Db.Administration.User.UserId","required":true,"insertable":false,"updatable":false,"alignment":"right","width":55,"editLink":true},{"name":"Username","title":"Db.Administration.User.Username","editorParams":{"maxLength":100},"maxLength":100,"required":true,"width":150,"editLink":true},{"name":"DisplayName","title":"Db.Administration.User.DisplayName","editorParams":{"maxLength":100},"maxLength":100,"required":true,"width":150},{"name":"Email","title":"Db.Administration.User.Email","editorParams":{"maxLength":100},"maxLength":100,"width":250},{"name":"Source","title":"Db.Administration.User.Source","editorParams":{"maxLength":4},"maxLength":4,"required":true,"insertable":false,"updatable":false,"defaultValue":"site","width":100}]);
+
+
+
---
+++ SereneXamarin/SereneXamarin.Web/Assets/FakeAjax.js
@@ -0,0 +1,57 @@
+﻿var FakeAjaxHandler = {};
+var FakeXHR = {
+    fail: function () {
+
+    },
+    abort: function () {
+
+    }
+};
+
+var submitFakeAjaxResponse = function (requestedUrl, responseJson) {
+    var response = JSON.parse(responseJson);
+    FakeAjaxHandler[requestedUrl].success(response, '200', FakeXHR);
+};
+
+var FakeAjax = (function () {
+    function FakeAjax() {
+        this.oldAjax = $.ajax;
+        Q.Config.applicationPath = 'fakeajax:';
+
+        var self = this;
+        this.handlers = {};
+        $.ajax = function (settings) {
+            //var url = settings.url;
+            //var handler = self.handlers[url];
+            //if (!handler) {
+            //    throw new Error("No fake handler registered for ajax URL: " + url + ", request: " +
+            //        JSON.stringify(settings, null, "    "));
+            //}
+
+
+
+            var jsonToURI = function (json) { return encodeURIComponent(JSON.stringify(json)); }
+            var uriToJSON = function (urijson) { return JSON.parse(decodeURIComponent(urijson)); }
+            if (settings.url && settings.url.substr(0, 2) === '~/') {
+                settings.url = 'FakeAjax:' + settings.substr(2);
+            }
+            var uri = settings.url + '?' + jsonToURI(settings.request);
+            window.location.href = uri;
+
+
+            FakeAjaxHandler[settings.url] = settings;
+            var xhr = FakeXHR;
+
+            return xhr;
+        };
+    }
+    FakeAjax.prototype.addServiceHandler = function (service, handler) {
+        this.handlers[Q.resolveUrl(service)] = handler;
+    };
+    FakeAjax.prototype.dispose = function () {
+        $.ajax = this.oldAjax;
+    };
+    return FakeAjax;
+}());
+
+FakeAjax();
